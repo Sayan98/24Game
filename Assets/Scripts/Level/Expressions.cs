79dd6b4c37327aace98b9d51bf8923d1f7e0c86d
Sayan Chanda <sayanc@suntechnologies.com>
2021-05-24 16:26:17 +0530

    Initial commit

---
+++ Assets/Scripts/Level/Expressions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NCalc;
+
+public class Expressions : MonoBehaviour {
+
+    [SerializeField] private Button[] numberButton;
+    [SerializeField] private Text[] numberText;
+
+    [SerializeField] private Text resultText, operationText;
+
+    private readonly char[] operatorSigns;
+
+    private string inputString;
+    
+    [SerializeField] private Text debugText;
+    
+
+    private Expressions() {
+
+        operatorSigns = new []{'(', ')', '+', '-', '*', '/'};
+    }
+
+    private void Awake() {
+        
+        resultText.text = operationText.text = "";
+    }
+
+    public void OperatorInput(int num) {
+
+        inputString += operatorSigns[num];
+        operationText.text = inputString;
+        
+        debugText.text = inputString;
+    }
+
+    public void NumberInput(int num) {
+
+        numberButton[num].interactable = false;
+
+        inputString += numberText[num].text;
+        operationText.text = inputString;
+        
+        debugText.text = inputString;
+
+        resultText.text = Calculation();
+        
+        //debugText.text = Calculation();
+    }
+
+    private string Calculation() {
+
+        var expression = new Expression(inputString);
+        return expression.Evaluate().ToString();
+    }
+
+    public void DeleteLastInput() {
+
+        inputString = inputString.Remove(inputString.Length - 1);
+        
+        debugText.text = inputString;
+
+        if (inputString.Length == 0) return;
+        operationText.text = inputString;
+        resultText.text = Calculation();
+        
+        
+    }
+}

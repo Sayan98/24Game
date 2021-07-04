c739c93de0b00633c43ec5f491dd9206f4ea6733
Sayan Chanda <mysayanchanda@gmail.com>
2021-07-04 18:15:09 +0530

    last commit

--- Assets/Scripts/Level/Expressions.cs
+++ Assets/Scripts/Level/Expressions.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.UI;
 using NCalc;
+using Random = UnityEngine.Random;
 
 public class Expressions : MonoBehaviour {
 
@@ -15,7 +19,7 @@ public class Expressions : MonoBehaviour {
     private readonly char[] operatorSigns;
 
     private string inputString;
-    
+
     [SerializeField] private Text debugText;
     
 
@@ -24,49 +28,119 @@ public class Expressions : MonoBehaviour {
         operatorSigns = new []{'(', ')', '+', '-', '*', '/'};
     }
 
+    
     private void Awake() {
         
         resultText.text = operationText.text = "";
+        inputString = null;
+
+        debugText.text = Calculation("2-1+3*2");
     }
 
+    #region Input
     public void OperatorInput(int num) {
 
+        if (inputString == null && num > 0) return;
+
+        if (inputString != null) {
+            
+            for (var i = 2; i < 6; i++)
+                if (operatorSigns[i].Equals(inputString[inputString.Length - 1])) {
+
+                    DeleteLastInput();
+                    break;
+                }
+
+            if (inputString[inputString.Length - 1].Equals(operatorSigns[0]))
+                return;
+        }
+        
         inputString += operatorSigns[num];
         operationText.text = inputString;
-        
-        debugText.text = inputString;
     }
 
+    
     public void NumberInput(int num) {
+        
+        if (!string.IsNullOrEmpty(inputString)) {
+
+            if ( inputString[inputString.Length - 1] > 48)
+                DeleteLastInput();
+        }
 
         numberButton[num].interactable = false;
 
         inputString += numberText[num].text;
         operationText.text = inputString;
-        
-        debugText.text = inputString;
 
-        resultText.text = Calculation();
-        
-        //debugText.text = Calculation();
+        resultText.text = Calculation(inputString);
+        StartCoroutine(CheckPositiveOutput());
     }
+    #endregion
 
-    private string Calculation() {
+    
+    
+    
+    private IEnumerator CheckPositiveOutput() {
+        
+        yield return new WaitForSecondsRealtime(0.5f);
+        
+        if (float.Parse(Calculation(inputString)) < 0 || float.Parse(Calculation(inputString)) % 1 != 0)
+            ResetLevel();
+    }
+    
+    
+    private string Calculation(string localInput) {
 
-        var expression = new Expression(inputString);
+        var expression = new Expression(localInput);
+        return Mathf.Abs(float.Parse(expression.Evaluate().ToString())).ToString(CultureInfo.InvariantCulture);
         return expression.Evaluate().ToString();
     }
 
-    public void DeleteLastInput() {
-
-        inputString = inputString.Remove(inputString.Length - 1);
+    
+    private void DeleteLastInput() {
         
-        debugText.text = inputString;
-
         if (inputString.Length == 0) return;
+        
+        for (var i = 0; i < 4; i++) {
+            
+            if (inputString[inputString.Length - 1].Equals(char.Parse(numberText[i].text))) 
+                numberButton[i].interactable = true;
+        }
+        
+        inputString = inputString.Remove(inputString.Length - 1);
+
+        if (inputString.Length == 0) {
+            
+            resultText.text = operationText.text = inputString;
+            return;
+        }
+        
+        debugText.text = inputString.Length + ":::::::";
+       
+        var signBalanced = inputString;
+        if (inputString[inputString.Length - 1] < 48)
+            signBalanced = inputString.Trim(inputString[inputString.Length - 1]);
+
+        resultText.text = Calculation(signBalanced);
         operationText.text = inputString;
-        resultText.text = Calculation();
-        
-        
     }
+
+
+    public void ResetLevel() {
+
+        resultText.text = operationText.text = inputString = "";
+        
+        for (var i = 0; i < 4; i++) 
+            numberButton[i].interactable = true;
+        inputString = null;
+    }
+
+    public void ShuffleNumber() {
+
+        for (var i = 0; i < 4; i++)
+            numberText[i].text = Random.Range(1, 10).ToString();
+    }
+    
+    
 }
---
+++ Assets/Scripts/Read.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Read : MonoBehaviour {
+   
+    public Text text;
+    private void Start() {
+        
+        var stt = File.ReadAllLines("Assets/Resources/3000 word list.txt");
+        text.text = stt[6];
+        
+    }
+}
